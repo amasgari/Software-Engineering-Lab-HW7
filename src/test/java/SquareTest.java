872b64ecb0212a9f2e7c238b5da32f9c4bac966a
Zahra Yousefi Jamarani <45602698+zahrayousefijamarani@users.noreply.github.com>
2022-05-25 12:35:12 +0430

    Add square test

---
+++ src/test/java/SquareTest.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RectangleTest {
+
+    @Test
+    public void getSideMethodTest() {
+        int side = 1;
+        Square square = new Square(side);
+        assertEquals(1, square.GetSide());
+    }
+
+    @Test
+    public void setSideMethodTest() {
+        int side = 1;
+        Square square = new Square(side);
+        rectangle.SetSide(2);
+        assertEquals(2, square.GetSide());
+    }
+
+    @Test
+    public void computeAreaNormal() {
+        int side = 2;
+        int expectedArea = 4;
+        Square square = new Square(side);
+        assertEquals(expectedArea, square.computeArea());
+    }
+
+    @Test
+    public void computeAreaNegSide() {
+        int side = 2;
+        try {
+            Square square = new Square(side);
+            square.computeArea();
+            Assert.fail();
+        }
+        catch (NegativeArgException e) {
+            System.out.println(e.getMessage());
+            assertTrue(true);
+        }
+    }
+
+}
